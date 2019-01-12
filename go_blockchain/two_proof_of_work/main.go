99015c2a2f88bacd5e521ed36dcbc03a522895a7
zhouxiaolong <whoiszxl@gmail.com>
2019-01-12 18:30:39 +0800

    【go公鏈】將序列化后的區塊存儲到本地文件

--- go_blockchain/two_proof_of_work/main.go
+++ go_blockchain/two_proof_of_work/main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"whoiszxl.com/BlackDream/go_blockchain/two_proof_of_work/blc"
-	"fmt"
 )
 
 
 func main() {
-	//data string,height int64,prevBlockHash []byte
-	block := blc.NewBlock("Test",1,[]byte{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
-
-	fmt.Println("--------------------")
-
-	bytes := block.Serialize()
-
-	fmt.Println(bytes)
-
-	block = blc.DeserializeBlock(bytes)
-
-	fmt.Printf("%d\n",block.Nonce)
-	fmt.Printf("%x\n",block.Hash)
+	// 创世区块
+	blockchain := blc.CreateBlockchainWithGenesisBlock()
+	defer blockchain.DB.Close()
 }
