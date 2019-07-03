56f56e5a238ebff6c35b1cc10bfd3e9c2f44b961
zhouxiaolong <whoiszxl@gmail.com>
2019-07-03 16:48:33 +0800

    字符串拼接的bug修复

--- ebay_dapp/Ebay/contracts/EcommerceStore.sol
+++ ebay_dapp/Ebay/contracts/EcommerceStore.sol
@@ -82,7 +82,7 @@ contract EcommerceStore {
         Product storage product = stores[productIdInStore[_productId]][_productId];
         require(now > product.auctionEndTime);
         //获取到加密后的key
-        bytes32 sealedBid = keccak256(bytes(concat(_amount, _secret)));
+        bytes32 sealedBid = keccak256(bytes(strConcat(_amount, _secret)));
 
         //通过加密key到商品的bids中拿到投标人的信息,并且需要地址存在，
         Bid memory bidInfo = product.bids[msg.sender][sealedBid];
@@ -165,25 +165,37 @@ contract EcommerceStore {
         return result; 
     }
     
-    function concat(string memory _base, string memory _value) public pure returns (string memory) {
-        bytes memory _baseBytes = bytes(_base);
-        bytes memory _valueBytes = bytes(_value);
+    /**
+     * 因为官方没有实现字符串拼接函数，故需自己实现
+     * 参考链接：https://ethereum.stackexchange.com/questions/729/how-to-concatenate-strings-in-solidity
+     */
+    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) internal pure returns (string memory){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        bytes memory _bc = bytes(_c);
+        bytes memory _bd = bytes(_d);
+        bytes memory _be = bytes(_e);
+        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
+        bytes memory babcde = bytes(abcde);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+        for (uint i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+        for (uint i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
+        for (uint i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
+        for (uint i = 0; i < _be.length; i++) babcde[k++] = _be[i];
+        return string(babcde);
+    }
 
-        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
-        bytes memory _newValue = bytes(_tmpValue);
-
-        uint i;
-        uint j;
-
-        for(i=0; i<_baseBytes.length; i++) {
-            _newValue[j++] = _baseBytes[i];
-        }
-
-        for(i=0; i<_valueBytes.length; i++) {
-            _newValue[j++] = _valueBytes[i++];
-        }
-
-        return string(_newValue);
+    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d) internal pure returns (string memory) {
+        return strConcat(_a, _b, _c, _d, "");
+    }
+    
+    function strConcat(string memory _a, string memory _b, string memory _c) internal pure returns (string memory) {
+        return strConcat(_a, _b, _c, "", "");
+    }
+    
+    function strConcat(string memory _a, string memory _b) internal pure returns (string memory) {
+        return strConcat(_a, _b, "", "", "");
     }
     
 }
